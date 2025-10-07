fbf42a501a347cfc22a6b9c7b780c1b5b3220e2a
Vlczakos <david@vlcak.com>
2025-10-07 12:09:09 +0200

    added rand crate

--- program2/src/main.rs
+++ program2/src/main.rs
@@ -3,9 +3,13 @@ Vystup:
 
 Program 2
 Toto je funkce v crate 1
+Nahodne cislo: <cislo>
 */
 
 fn main() {
     println!("Program 2");
     crate1::test_function();
+
+    let i = rand::random::<u32>();
+    println!("Nahodne cislo: {}", i);
 }
